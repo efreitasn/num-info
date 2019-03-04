28c7b7733182625702d5de2c4f51c0418fe597f6
Emanuel Freitas <efreitasn@hotmail.com>
2019-03-04 13:07:08 -0300

    Initial commit

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+export default function App() {
+  return (
+    <main className="app">
+      <header>
+        <h1>hooksTodo</h1>
+      </header>
+      <div className="main-box">
+        the-content
+      </div>
+    </main>
+  );
+}

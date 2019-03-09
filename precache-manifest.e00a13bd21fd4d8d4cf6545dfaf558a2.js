9ca786a22fd10d55b78b2131bafe9b7cea2cbe9e
Emanuel Freitas <efreitasn@hotmail.com>
2019-03-09 20:40:23 -0300

    Updates

---
+++ precache-manifest.e00a13bd21fd4d8d4cf6545dfaf558a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "959e824f0ef4957e874e",
+    "url": "/num-info/static/js/runtime~main.959e824f.js"
+  },
+  {
+    "revision": "c369fc46e91657722fda",
+    "url": "/num-info/static/js/main.c369fc46.chunk.js"
+  },
+  {
+    "revision": "f609d8845b319229b686",
+    "url": "/num-info/static/js/2.f609d884.chunk.js"
+  },
+  {
+    "revision": "c369fc46e91657722fda",
+    "url": "/num-info/static/css/main.db4f4ee8.chunk.css"
+  },
+  {
+    "revision": "27bacf2956fe6a9e626b784fb88ca671",
+    "url": "/num-info/index.html"
+  }
+];

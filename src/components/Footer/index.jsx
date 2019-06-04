a06927f1618fb532c7b1372d664b970196b65a92
Emanuel Freitas <efreitasn@hotmail.com>
2019-06-04 11:10:37 -0300

    Update Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,23 +4,19 @@ import './styles.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div>
-        <a
-          className="footer-link"
-          href="https://github.com/efreitasn/num-info"
-          target="_blank"
-          rel="noopener noreferrer"
-        >View on github</a>
-      </div>
-      <div className="footer-author-info">
-        <span className="footer-by">By </span>
-        <a
-          className="footer-link"
-          href="https://efreitasn.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >efreitasn</a>
-      </div>
+      <a
+        className="footer-link"
+        href="https://github.com/efreitasn/num-info"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Github</a>
+      <span class="footer-separator">/</span>
+      <a
+        className="footer-link"
+        href="https://efreitasn.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >efreitasn</a>
     </footer>
   );
 }
